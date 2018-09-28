5fca8db0a09b04d6e78b6a241bc57eb3da8f46bc
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-28 19:46:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(:username => params[:username])
 
-    
+    if user && user.authenticate(params[:password])
   end
 
   get "/failure" do
